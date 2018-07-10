09cb06d37d2711d187c0658eb141f0d122a32d59
Dat Nguyen <dat.nguyen@ge.com>
2018-07-10 16:51:36 -0700

    fixing heroku server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-app.use(express.static(__dirname +  '/public'));
+app.use(express.static(__dirname +  '/'));
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/public/index.html'));
-// });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 let hostname = 'localhost';
 let port = process.env.PORT || 3000;
